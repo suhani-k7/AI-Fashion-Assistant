90941fca6c9156503ac1819f01e55eea5899d1f0
Suhani Khosla <suhanikhosla2@gmail.com>
2026-04-02 18:51:37 +0530

    image upload and API integration working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import axios from "axios";
+
 import './App.css'
 
 function App() {
+  const [image,setImage]=useState(null);
+  const [budget, setBudget]=useState("");
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const formData=new FormData();
+    formData.append("image",image);
+    formData.append("budget",budget);
+    try{
+      const res=await axios.post(
+        "http://localhost:8000/upload",
+        formData
+      );
+      console.log(res.data);
+    }catch (err){
+      console.error(err);
+    }
+  }
   return(
     <div style={{padding:"20px"}}>
       <h1>AI FASHION ASSISTANT</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          onChange={(e)=> setImage(e.target.files[0])}
+          required
+        />
+        <br /><br />       
+        <input
+          type="number"
+          placeholder="Enter budget"
+          value={budget}
+          onChange={(e) => setBudget(e.target.value)}
+          required
+        />
+        <br /><br />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express=require("express");
 const cors=require("cors");
+const multer=require("multer");
+
+const upload=multer({dest:"uploads/"});
 const app=express();
 app.use(cors());
 app.use(express.json());
@@ -9,3 +12,21 @@ app.get("/", (req,res)=>{
 app.listen(8000,()=>{
     console.log("Server running on port 8000");
 })
+
+app.post("/upload", upload.single("image"), (req,res) =>{
+    try{
+        console.log("File:", req.file);
+        console.log("Budget:", budget);
+        res.json({
+            message: "data received succesfully",
+        });
+    } catch (error) {
+        console.error("ERROR:",error);
+        res.status(500).json({
+            message: "Error occurred while processing the request",
+        });
+    }
+app.get("/upload", (req, res) => {
+  res.send("Upload route working (GET)");
+});
+});
